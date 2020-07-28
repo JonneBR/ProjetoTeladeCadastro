d0022df9c29a6e005678fc9de08f25ed3eb1e850
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2020-07-28 12:29:36 -0300

    Input  Correto ou Incorreto

--- script.js
+++ script.js
@@ -2,13 +2,29 @@ const form = document.getElementById('form');
 
 const usuario = document.getElementById('usuario');
 const email = document.getElementById('email');
+const senha = document.getElementById('senha');
+const confirmarSenha = document.getElementById('senhaRepetir');
 
-function checarCampos(dadosUsuario) {
-  dadosUsuario.forEach(function (input) {
-    if (input.value.trim() === '') {
-      alert('erro teste');
+function exibirCamposIncorretos(inputsIncorretos) {
+  const formcontrol = inputsIncorretos.parentElement;
+  formcontrol.className = 'form-control CadastroFalhou';
+}
+
+function exibirCamposCorretos(inputsCorretos) {
+  const formControl = inputsCorretos.parentElement;
+  formControl.className = 'form-control CadastroSucesso';
+  //console.log(formControl);
+}
+
+function checarCampos(inputUsuario) {
+  //console.log(inputUsuario);
+  inputUsuario.forEach(function (inputUsuario) {
+    //console.log(inputUsuario.value.trim());
+    if (inputUsuario.value.trim() === '') {
+      exibirCamposIncorretos(inputUsuario);
+      console.log('Erro');
     } else {
-      alert('SUCESSO');
+      exibirCamposCorretos(inputUsuario);
     }
   });
 }
@@ -17,5 +33,5 @@ form.addEventListener('submit', function (e) {
   //alert(form);
   e.preventDefault();
 
-  checarCampos([usuario, email]);
+  checarCampos([usuario, email, senha, confirmarSenha]);
 });
